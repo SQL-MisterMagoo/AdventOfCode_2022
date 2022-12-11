2181dd6a24810e279aa35bc4c2630bca7a62b9c1
Mister Magoo <mister.magoo+githubsql@gmail.com>
2022-12-11 01:07:19 +0000

    Add project files.

---
+++ Puzzle_01_Part_01/Program.cs
@@ -0,0 +1,11 @@
+﻿int c=0;
+var result = 
+	File
+		.ReadAllLines("PuzzleInput.txt")
+		.Select((v, i) => v is string x && x.Length == 0 && ++c > 0 ? new { c, v } : new { c, v })
+		.Where(t => t.v is string tv && tv.Length > 0)
+		.GroupBy(x => x.c)
+		.Select(g => new { ElfNumber = g.Key, Calories = g.Sum(c => int.Parse(c.v)) })
+		.MaxBy(g => g.Calories);
+	
+Console.WriteLine($"{result.ElfNumber},{result.Calories}");
