4913aad65143d44cd56dc49701c793a17def9dcd
Mister Magoo <mister.magoo+githubsql@gmail.com>
2022-12-12 00:16:04 +0000

    Day 2 Part 1

---
+++ Puzzle_02_Part_01/Program.cs
@@ -0,0 +1,17 @@
+﻿var result =
+	File.ReadAllLines("PuzzleInput.txt")
+	.Sum(i =>
+		(i[0], i[2]) switch 
+		{
+			('A','X') => 1 + 3,
+			('B','Y') => 2 + 3,
+			('C','Z') => 3 + 3,
+			('A','Y') => 2 + 6,
+			('B','Z') => 3 + 6,
+			('C','X') => 1 + 6,
+			('A','Z') => 3 + 0,
+			('B','X') => 1 + 0,
+			('C','Y') => 2 + 0,
+			(_,_) => 0
+	});
+Console.WriteLine(result);
