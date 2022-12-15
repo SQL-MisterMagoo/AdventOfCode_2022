4a9e27d01af73bc6bac5c78650cd96ab0c683703
Mister Magoo <mister.magoo+githubsql@gmail.com>
2022-12-15 00:58:19 +0000

    Day 4 Part 2

---
+++ Puzzle_04_Part_02/Program.cs
@@ -0,0 +1,18 @@
+﻿//File.ReadAllLines("Practice.txt")
+var result =
+	File.ReadAllLines("PuzzleInput.txt")
+		.Select(
+			line=> (
+				left: line.Split(',')[0].Split('-').Select(range=>int.Parse(range)).ToArray(), 
+				right: line.Split(',')[1].Split('-').Select(range => int.Parse(range)).ToArray()
+				)
+			)
+		.Where(
+			elves=> !(
+				(elves.left[0] > elves.right[1])
+				||
+				(elves.left[1] < elves.right[0])
+				)
+		)
+		.Count();
+	Console.WriteLine(result);
